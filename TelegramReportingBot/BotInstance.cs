955e151023631cb45cc5ef402898f00e2c311758
Jay VanOrd <jay.vanord@maplarge.com>
2024-09-04 16:07:20 -0400

    scaffolded bot instance

---
+++ TelegramReportingBot/BotInstance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramReportingBot {
+	internal class BotInstance {
+		private string _token;
+		private Logger _logger;
+
+		BotInstance(string token, Logger logger) {
+			_token = token;
+			_logger = logger; 
+		}
+
+		internal async void StartPolling() {
+			using var cts = new CancellationTokenSource();
+			var client = new TelegramBotClient(_token, cancellationToken: cts.Token);
+			var botIdentity = await client.GetMeAsync();
+			await client.DropPendingUpdatesAsync();
+			client.OnMessage += async (message, updateType) => {
+				await Task.Delay(1000);
+			};
+			client.OnUpdate += async (update) => {
+				await Task.Delay(1000);
+			};
+			client.OnError += (exception, source) => {
+				throw exception;
+			};
+		}
+	}
+}
